c330668c9329b4161473f5b4580988b85c029040
Sachin Varghese <sachinv2223@gmail.com>
2022-07-05 00:34:39 +0530

    day2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstWork';
+  public heading = 'IT MASTER';
+  data = {
+    tech1: "Angular",
+    tech2: "React",
+    tech3: "Vue"
+  }
+  colorFuntion(){
+    return "yellow";
+  }
+  alignFunction(){
+    return "center";
+  }
+  heightFunction(){
+    return "25px";
+  }
+  colorFunction(){
+    return "red";
+  }
+  public msg="";
+  addMorningMsg(){
+    console.log("Good Morning")
+    this.msg = "Good Morning";
+  }
+  addNightMsg(){
+    console.log("Good Night")
+    this.msg = "Good Night";
+  }
+  public count = 0;
+  addOne(){
+    this.count++;
+    if((this.count%2)==0){
+      this.status="Even"
+    }
+    else{
+      this.status="Odd"
+    }
+  }
+  subtractOne(){
+    this.count--;
+    if((this.count%2)==0){
+      this.status="Even"
+    }
+    else{
+      this.status="Odd"
+    }
+  }
+  public status="Even";
 }
